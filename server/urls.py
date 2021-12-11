ef20e56ecc3d05a915d85a14d30d73798338ffa7
Wrexan <fwrexan@gmail.com>
2021-12-11 22:27:30 +0200

    remove junk

--- server/urls.py
+++ server/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from users.views import UserModelViewSet
 from usersapp.views import UserModelViewSet
 
 router = DefaultRouter()
-router.register('authors', UserModelViewSet)
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
