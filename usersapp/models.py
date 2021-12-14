95e2df4a7bd1f678893aca0dca67939992e45e67
Wrexan <fwrexan@gmail.com>
2021-12-14 18:07:34 +0200

    finally, redone to AbstractUser

--- usersapp/models.py
+++ usersapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    user_name = models.CharField(max_length=64, unique=True)
+    username = models.CharField(max_length=64, unique=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField(max_length=64, unique=True)
--- usersapp/serializers.py
+++ usersapp/serializers.py
@@ -5,4 +5,7 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'email',)
