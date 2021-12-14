7789c8c01ee72f9893b388132510d0a2e1ff89d6
Wrexan <fwrexan@gmail.com>
2021-12-14 21:09:47 +0200

    fixed fill_db

--- usersapp/management/commands/fill_db.py
+++ usersapp/management/commands/fill_db.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User as Admin
 
 from usersapp.models import User
 
@@ -17,15 +16,34 @@ def load_from_json(file_name):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        admin_absent = True
+        users_num = 0
         users = load_from_json(JSON_NAME)
 
         User.objects.all().delete()
         for user in users:
-            new_user = User(**user['fields'])
-            new_user.pk = user['pk']  # comment to prevent over-wright items in db (if they exist)
+            # new_user = User(**user['fields'])
+            new_user = User.objects.create_user(id=user['pk'],
+                                                username=user['fields']['username'],
+                                                first_name=user['fields']['first_name'],
+                                                last_name=user['fields']['last_name'],
+                                                email=user['fields']['email'],
+                                                password='123',  # user['fields']['password'],
+                                                is_superuser=user['fields']['is_superuser'],
+                                                is_staff=user['fields']['is_staff'],
+                                                is_active=user['fields']['is_active'],
+                                                last_login=user['fields']['last_login'],
+                                                date_joined=user['fields']['date_joined'],
+                                                )
+            new_user.groups.set(user['fields']['groups'])
+            new_user.user_permissions.set(user['fields']['user_permissions'])
+            if new_user.is_superuser: admin_absent = False
             new_user.save()
+            users_num += 1
+        print(f"Loaded users: {users_num}")
 
-        Admin.objects.all().delete()
-        super_user = Admin.objects.create_superuser('Admin', 'x@x.x', '123')
-        if super_user:
+        # Admin.objects.all().delete()
+        if admin_absent:
+            super_user = User.objects.create_superuser('admin', 'x@x.x', '123')
+            super_user.save()
             print("Super user created.")
